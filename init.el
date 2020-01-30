d2eb5ba6ace50685d8671460929b40e62b9501b6
AxelBohm <axel.boehm@ymail.com>
2020-01-30 18:53:04 +0100

    formatting

--- init.el
+++ init.el
@@ -107,9 +107,9 @@
        ;;erlang            ; an elegant language for a more civilized age
        ;;elixir            ; erlang done right
        ;;elm               ; care for a cup of TEA?
-       emacs-lisp        ; drown in parentheses
-       ess               ; emacs speaks statistics
-       ;;fsharp           ; ML stands for Microsoft's Language
+       emacs-lisp          ; drown in parentheses
+       ess                 ; emacs speaks statistics
+       ;;fsharp            ; ML stands for Microsoft's Language
        ;;go                ; the hipster dialect
        ;;(haskell +intero) ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
