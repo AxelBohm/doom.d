bc634ebcd91701b0e03688b50417dad55121f35a
AxelBohm <axel.boehm@ymail.com>
2021-07-06 09:47:10 +0200

    new package deft for searching roam notes

--- packages.el
+++ packages.el
@@ -8,7 +8,6 @@
 
 (package! org-wild-notifier)
 (package! evil-matchit)
-(package! org-roam-bibtex)
 (package! org-ref)
 (package! company-bibtex)
 (package! helm-bibtex :pin "12f8809aac3a13dd11a1c664a13f789005f7a199")
@@ -16,4 +15,3 @@
 
 (package! doom-quit :disable t)
 (package! doom-snippets :disable t)
-(unpin! org-mode) ;; bugfix
