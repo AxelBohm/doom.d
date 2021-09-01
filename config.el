03d68b0f51dc08979bb1578313ba3e1bff59537b
AxelBohm <axel.boehm@ymail.com>
2021-09-01 14:23:20 +0200

    headings normal size

--- config.el
+++ config.el
@@ -264,21 +264,17 @@
 (after! org
   (setq-hook! 'org-mode-hook +flyspell-immediately nil))
 
-(after! org
-  (custom-set-faces
-   '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
-   '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
-   '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
-   '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
-   '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
-   ))
+;; (after! org
+;;   (custom-set-faces
+;;    '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+;;    '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
+;;    '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+;;    '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+;;    '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+;;    ))
 
 (after! org
   (setq org-ellipsis " ..."))
-
-;; (after! org
-;;   (setq org-superstar-mode 1))
-;; (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
 
 (after! org
   (setq org-directory "~/org")
